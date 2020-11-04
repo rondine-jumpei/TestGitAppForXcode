5019883bf71c75a4ee48b98bd3fec0af5a670110
rondine-jumpei <pioggia526@icloud.com>
2020-11-04 23:16:37 +0900

    first commit

---
+++ TestGitAppForXcode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestGitAppForXcode
+//
+//  Created by ろーんでぃね on 2020/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("xcodeでのgithubアップロードテスト")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
